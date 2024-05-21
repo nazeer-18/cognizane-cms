c321f9c41bef6254b7c2ebe5de195fd6b98f230a
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2024-05-22 02:42:13 +0530

    added responsiveness to navbar yet to display the navigations

--- cognizance-cms/src/components/Navbar.js
+++ cognizance-cms/src/components/Navbar.js
@@ -9,22 +9,28 @@ export default function Navbar() {
         <div className="navbar">
             <div className="navbar-cognizance">
                 <div className="navbar-logo">
-                    <img src={logo} alt="Cognizance Logo" />
+                    <Link to="/">
+                        <img src={logo} alt="Cognizance Logo" />
+                    </Link>
                 </div>
                 <div className="navbar-title">
                     Cognizance
                 </div>
             </div>
             <div className="navbar-navigation">
-                <ul className="navbar-list">
-                    <li className="navbar-link"> <Link exact to="/aboutus"> About Us </Link></li>
-                    <li className="navbar-link"> <Link exact to="/events"> Events </Link></li>
-                    <li className="navbar-link"> <Link exact to="/domains"> Domains </Link></li>
-                    <li className="navbar-link"> <Link exact to="/alumini"> Alumini </Link></li>
-                    <li className="navbar-link"> <Link exact to="/gallery"> Gallery </Link></li>
-                    <li className="navbar-link"> <Link exact to="/contactus"> Contact Us </Link></li>
-                    <li className="navbar-link"> <img className='navbar-user' src={user} alt="" /> </li>
-                </ul>
+                <div className="navbar-links-container">
+                    <ul className="navbar-list">
+                        <li className="navbar-link"> <Link exact to="/aboutus"> About Us </Link></li>
+                        <li className="navbar-link"> <Link exact to="/events"> Events </Link></li>
+                        <li className="navbar-link"> <Link exact to="/domains"> Domains </Link></li>
+                        <li className="navbar-link"> <Link exact to="/alumini"> Alumini </Link></li>
+                        <li className="navbar-link"> <Link exact to="/gallery"> Gallery </Link></li>
+                        <li className="navbar-link"> <Link exact to="/contactus"> Contact Us </Link></li>
+                    </ul>
+                </div>
+                <div className="navbar-user-container">
+                    <img className='navbar-user' src={user} alt="" />
+                </div>
             </div>
         </div>
     )
